87c5c7874d12347405aa3f46c4e5b5c6ec48edcc
naba_7 <taehyung3459@gmail.com>
2025-05-12 16:18:29 +0900

    병원리스트 mock데이터 수정(내과,치과)

--- src/pages/api/recommendation.ts
+++ src/pages/api/recommendation.ts
@@ -1,5 +1,5 @@
 // 백엔드 fetch 테스트용 임시파일
 export default function handler(req, res) {
-  res.status(200).json({ recommended_department: '내과' });
+  res.status(200).json({ recommended_department: '치과' });
 }
     
